b8a8608d34bb73c5b139fb778dc9c3c43b57c3c2
ubaid-ullah <ubaidullah31357@gmail.com>
2026-09-07 18:11:35 +0500

    Added the middleware logic in app, on how to use middlewares for a single route and for every route specified.

--- index.ts
+++ index.ts
@@ -1,11 +1,25 @@
 import express, { type Express, type Request, type Response } from "express";
-import posts from "./posts";
+import userAuthentication from "./middlewares/userAuthentication";
 
 const app: Express = express();
 const port = 3000;
 
-app.get("/posts", (req: Request, res: Response) => {
-  res.json(posts);
+// For a single route, you can also apply the middleware like this:
+app.get("/protected", userAuthentication, (req: Request, res: Response) => {
+  res.send("This is a protected route that requires user authentication!");
+});
+
+// Use the user authentication middleware for all routes
+app.use(userAuthentication);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(
+    "Welcome to the Express server with user authentication middleware!",
+  );
+});
+
+app.get("/about", (req: Request, res: Response) => {
+  res.send("About page with user authentication middleware!");
 });
 
 app.listen(port, () => {
