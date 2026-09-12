a83dd8823f6f31094cc8b3d55ba002d7267a12aa
ubaid-ullah <ubaidullah31357@gmail.com>
2026-09-12 10:45:05 +0500

    Added the code for how to deploy static pages on the home route using built-in middleware (express.static).

--- index.ts
+++ index.ts
@@ -4,24 +4,17 @@ import express, {
   type NextFunction,
   type Express,
 } from "express";
+import path from "path";
 
 const app: Express = express();
 
-app.get("/users", (req, res, next) => {
-  const error = new Error("Database failed");
-  next(error);
-});
+app.use(express.static("./public"));
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
+app.get("/", (req: Request, res: Response) => {
+  console.log("Static Pages Loaded.");
+  // res.sendFile(path.join(process.cwd(), "./public/index.html"));
 });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
-//
-// Define error-handling middleware functions in the same way as other middleware functions, except with four arguments instead of three, specifically with the signature (err, req, res, next).
-
-// You must provide four arguments to identify it as an error-handling middleware function. Even if you don’t need to use the next object, you must specify it to maintain the signature. Otherwise, the next object will be interpreted as regular middleware and will fail to handle errors.
---
+++ public/script.js
@@ -0,0 +1,3 @@
+const button = document.querySelector("button");
+console.log("Script loaded.");
+button?.addEventListener("click", () => console.log("Button clicked"));
